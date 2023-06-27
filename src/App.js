5420ebd44486dcccb0502c0e72404b11920006e4
Alex Zhiryakov <1234567elena@mail.ru>
2023-06-27 17:36:00 +0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,301 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const summa = 0
+
+  const hendler = () => {
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<>
+    <h2 className='parter'>Партер</h2>
+
+    <div className='price'>
+
+      <div className='first-prise'>
+        <div className='first-color'></div>
+        <div className='first-text'>49 $</div>
+      </div>
+
+      <div className='second-prise'>
+      <div className='second-color'></div>
+      <div className='second-text'>65 $</div>
+      </div>
+
+      <div className='third-prise'>
+      <div className='third-color'></div>
+      <div className='third-text'>89 $</div>
+      </div>
+
+      <div className='fourth-prise'>
+      <div className='fourth-color'></div>
+      <div className='fourth-text'>109 $</div>
+      </div>
+
+      <div className='fifth-prise'>
+      <div className='fifth-color'></div>
+      <div className='fifth-text'>145 $</div>
+      </div>
+
     </div>
-  );
+
+
+                      {/* // места */}
+
+    <div className='place'>
+
+
+      <div className='place-1'>
+          <div className='place-1-text'>Ряд 1</div>
+          <div className='place-color-1'>
+            <div onClick={hendler} className='place-red'></div>
+            <div onClick={hendler} className='place-red'></div>
+            <div onClick={hendler} className='place-red'></div>
+            <div onClick={hendler} className='place-red'></div>
+            <div onClick={hendler} className='place-red'></div>
+            <div onClick={hendler} className='place-red'></div>
+            <div onClick={hendler} className='place-red'></div>
+            <div onClick={hendler} className='place-red'></div>
+            <div onClick={hendler} className='place-red'></div>
+            <div onClick={hendler} className='place-red'></div>
+            <div onClick={hendler} className='place-red'></div>
+          </div>
+      </div>
+
+      <div className='place-2'>
+          <div className='place-2-text'>Ряд 2</div>
+          <div className='place-color-2'>
+            <div onClick={hendler} className='place-red-2'></div>
+            <div onClick={hendler} className='place-red-2'></div>
+            <div onClick={hendler} className='place-red-2'></div>
+            <div onClick={hendler} className='place-red-2'></div>
+            <div onClick={hendler} className='place-red-2'></div>
+            <div onClick={hendler} className='place-red-2'></div>
+            <div onClick={hendler} className='place-red-2'></div>
+            <div onClick={hendler} className='place-red-2'></div>
+            <div onClick={hendler} className='place-red-2'></div>
+            <div onClick={hendler} className='place-red-2'></div>
+            <div onClick={hendler} className='place-red-2'></div>
+            <div onClick={hendler} className='place-red-2'></div>
+            <div onClick={hendler} className='place-red-2'></div>
+          </div>
+      </div>
+
+      <div className='place-3'>
+         <div className='place-3-text'>Ряд 3</div>
+         <div className='place-color-3'>
+            <div onClick={hendler} className='place-yellow'></div>
+            <div onClick={hendler} className='place-yellow'></div>
+            <div onClick={hendler} className='place-yellow'></div>
+            <div onClick={hendler} className='place-yellow'></div>
+            <div onClick={hendler} className='place-yellow'></div>
+            <div onClick={hendler} className='place-yellow'></div>
+            <div onClick={hendler} className='place-yellow'></div>
+            <div onClick={hendler} className='place-yellow'></div>
+            <div onClick={hendler} className='place-yellow'></div>
+            <div onClick={hendler} className='place-yellow'></div>
+            <div onClick={hendler} className='place-yellow'></div>
+            <div onClick={hendler} className='place-yellow'></div>
+            <div onClick={hendler} className='place-yellow'></div>
+            <div onClick={hendler} className='place-yellow'></div>
+            <div onClick={hendler} className='place-yellow'></div>
+          </div>
+      </div>
+
+      <div className='place-4'>
+         <div className='place-4-text'>Ряд 4</div>
+         <div className='place-color-4'>
+            <div onClick={hendler} className='place-yellow-2'></div>
+            <div onClick={hendler} className='place-yellow-2'></div>
+            <div onClick={hendler} className='place-yellow-2'></div>
+            <div onClick={hendler} className='place-yellow-2'></div>
+            <div onClick={hendler} className='place-yellow-2'></div>
+            <div onClick={hendler} className='place-yellow-2'></div>
+            <div onClick={hendler} className='place-yellow-2'></div>
+            <div onClick={hendler} className='place-yellow-2'></div>
+            <div onClick={hendler} className='place-yellow-2'></div>
+            <div onClick={hendler} className='place-yellow-2'></div>
+            <div onClick={hendler} className='place-yellow-2'></div>
+            <div onClick={hendler} className='place-yellow-2'></div>
+            <div onClick={hendler} className='place-yellow-2'></div>
+            <div onClick={hendler} className='place-yellow-2'></div>
+            <div onClick={hendler} className='place-yellow-2'></div>
+          </div>
+      </div>
+
+      <div className='place-5'>
+         <div className='place-5-text'>Ряд 5</div>
+         <div className='place-color-5'>
+            <div onClick={hendler} className='place-purple'></div>
+            <div onClick={hendler} className='place-purple'></div>
+            <div onClick={hendler} className='place-purple'></div>
+            <div onClick={hendler} className='place-purple'></div>
+            <div onClick={hendler} className='place-purple'></div>
+            <div onClick={hendler} className='place-purple'></div>
+            <div onClick={hendler} className='place-purple'></div>
+            <div onClick={hendler} className='place-purple'></div>
+            <div onClick={hendler} className='place-purple'></div>
+            <div onClick={hendler} className='place-purple'></div>
+            <div onClick={hendler} className='place-purple'></div>
+            <div onClick={hendler} className='place-purple'></div>
+            <div onClick={hendler} className='place-purple'></div>
+            <div onClick={hendler} className='place-purple'></div>
+            <div onClick={hendler} className='place-purple'></div>
+            <div onClick={hendler} className='place-purple'></div>
+            <div onClick={hendler} className='place-purple'></div>
+          </div>
+      </div>
+
+      <div className='place-6'>
+         <div className='place-6-text'>Ряд 6</div>
+         <div className='place-color-6'>
+            <div onClick={hendler} className='place-purple-2'></div>
+            <div onClick={hendler} className='place-purple-2'></div>
+            <div onClick={hendler} className='place-purple-2'></div>
+            <div onClick={hendler} className='place-purple-2'></div>
+            <div onClick={hendler} className='place-purple-2'></div>
+            <div onClick={hendler} className='place-purple-2'></div>
+            <div onClick={hendler} className='place-purple-2'></div>
+            <div onClick={hendler} className='place-purple-2'></div>
+            <div onClick={hendler} className='place-purple-2'></div>
+            <div onClick={hendler} className='place-purple-2'></div>
+            <div onClick={hendler} className='place-purple-2'></div>
+            <div onClick={hendler} className='place-purple-2'></div>
+            <div onClick={hendler} className='place-purple-2'></div>
+            <div onClick={hendler} className='place-purple-2'></div>
+            <div onClick={hendler} className='place-purple-2'></div>
+            <div onClick={hendler} className='place-purple-2'></div>
+            <div onClick={hendler} className='place-purple-2'></div>
+          </div>
+      </div>
+
+      <div className='place-7'>
+         <div className='place-7-text'>Ряд 7</div>
+         <div className='place-color-7'>
+            <div onClick={hendler} className='place-orange'></div>
+            <div onClick={hendler} className='place-orange'></div>
+            <div onClick={hendler} className='place-orange'></div>
+            <div onClick={hendler} className='place-orange'></div>
+            <div onClick={hendler} className='place-orange'></div>
+            <div onClick={hendler} className='place-orange'></div>
+            <div onClick={hendler} className='place-orange'></div>
+            <div onClick={hendler} className='place-orange'></div>
+            <div onClick={hendler} className='place-orange'></div>
+            <div onClick={hendler} className='place-orange'></div>
+            <div onClick={hendler} className='place-orange'></div>
+            <div onClick={hendler} className='place-orange'></div>
+            <div onClick={hendler} className='place-orange'></div>
+            <div onClick={hendler} className='place-orange'></div>
+            <div onClick={hendler} className='place-orange'></div>
+            <div onClick={hendler} className='place-orange'></div>
+            <div onClick={hendler} className='place-orange'></div>
+            <div onClick={hendler} className='place-orange'></div>
+            <div onClick={hendler} className='place-orange'></div>
+          </div>
+      </div>
+
+      <div className='place-8'>
+         <div className='place-8-text'>Ряд 8</div>
+         <div className='place-color-8'>
+            <div onClick={hendler} className='place-orange-2'></div>
+            <div onClick={hendler} className='place-orange-2'></div>
+            <div onClick={hendler} className='place-orange-2'></div>
+            <div onClick={hendler} className='place-orange-2'></div>
+            <div onClick={hendler} className='place-orange-2'></div>
+            <div onClick={hendler} className='place-orange-2'></div>
+            <div onClick={hendler} className='place-orange-2'></div>
+            <div onClick={hendler} className='place-orange-2'></div>
+            <div onClick={hendler} className='place-orange-2'></div>
+            <div onClick={hendler} className='place-orange-2'></div>
+            <div onClick={hendler} className='place-orange-2'></div>
+            <div onClick={hendler} className='place-orange-2'></div>
+            <div onClick={hendler} className='place-orange-2'></div>
+            <div onClick={hendler} className='place-orange-2'></div>
+            <div onClick={hendler} className='place-orange-2'></div>
+            <div onClick={hendler} className='place-orange-2'></div>
+            <div onClick={hendler} className='place-orange-2'></div>
+            <div onClick={hendler} className='place-orange-2'></div>
+            <div onClick={hendler} className='place-orange-2'></div>
+          </div>
+      </div>
+
+      <div className='place-9'>
+          <div className='place-9-text'>Ряд 9</div>
+          <div className='place-color-9'>
+            <div onClick={hendler} className='place-blue'></div>
+            <div onClick={hendler} className='place-blue'></div>
+            <div onClick={hendler} className='place-blue'></div>
+            <div onClick={hendler} className='place-blue'></div>
+            <div onClick={hendler} className='place-blue'></div>
+            <div onClick={hendler} className='place-blue'></div>
+            <div onClick={hendler} className='place-blue'></div>
+            <div onClick={hendler} className='place-blue'></div>
+            <div onClick={hendler} className='place-blue'></div>
+            <div onClick={hendler} className='place-blue'></div>
+            <div onClick={hendler} className='place-blue'></div>
+            <div onClick={hendler} className='place-blue'></div>
+            <div onClick={hendler} className='place-blue'></div>
+            <div onClick={hendler} className='place-blue'></div>
+            <div onClick={hendler} className='place-blue'></div>
+            <div onClick={hendler} className='place-blue'></div>
+            <div onClick={hendler} className='place-blue'></div>
+            <div onClick={hendler} className='place-blue'></div>
+            <div onClick={hendler} className='place-blue'></div>
+          </div>
+      </div>
+
+      <div className='place-10'>
+         <div className='place-10-text'>Ряд 10</div>
+         <div className='place-color-10'>
+            <div className='place-blue-2'></div>
+            <div className='place-blue-2'></div>
+            <div className='place-blue-2'></div>
+            <div className='place-blue-2'></div>
+            <div className='place-blue-2'></div>
+            <div className='place-blue-2'></div>
+            <div className='place-blue-2'></div>
+            <div className='place-blue-2'></div>
+            <div className='place-blue-2'></div>
+            <div className='place-blue-2'></div>
+            <div className='place-blue-2'></div>
+            <div className='place-blue-2'></div>
+            <div className='place-blue-2'></div>
+            <div className='place-blue-2'></div>
+            <div className='place-blue-2'></div>
+            <div className='place-blue-2'></div>
+            <div className='place-blue-2'></div>
+            <div className='place-blue-2'></div>
+            <div className='place-blue-2'></div>
+          </div>
+      </div>
+
+                  {/* оплата */}
+
+    </div>
+
+    <footer>
+
+    <div className='pay'>
+
+      <div className='total'>1 билет</div>
+      <div className='summa'>{summa} $</div>
+
+    </div>
+
+    <div className='pay-2'>
+
+      <div className='kupit'>Купить</div>
+
+    </div>
+
+
+    </footer>
+</>
+  )
 }
+
 
 export default App;
